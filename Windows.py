27e6933a7214c7a497325e4e9e8dc2f0022ce089
Nybashirka <ilabon60@gmail.com>
2022-10-22 01:17:44 +0900

    Добавил возможность списочного ввода координат для окна (словесный переделал на русские)

--- Windows.py
+++ Windows.py
@@ -15,15 +15,22 @@ class BaseWindow(QWidget):
         self.setWindowTitle(title)
 
     def locate_calc(self, locate, *size):
-        if type(locate) == type(tuple) or type(locate) == type(list):
+        if type(locate) == type(tuple()) or type(locate) == type(list()):
             return locate
         width = GetSystemMetrics(0)
         height = GetSystemMetrics(1)
-        locates = {"в центр": self._cordsCenter,
-                   "сверху": self._cordsTop,
-                   "снизу": self._cordsBelow,
-                   "справа": self._cordsLeft,
-                   "слева": self._cordsRight}
+        locates = {}
+        locates_rus = {"в центр": self._cordsCenter,
+                       "сверху": self._cordsTop,
+                       "снизу": self._cordsBelow,
+                       "справа": self._cordsLeft,
+                       "слева": self._cordsRight}
+        locates_eng = {"center": self._cordsCenter,
+                       "top": self._cordsTop,
+                       "below": self._cordsBelow,
+                       "left": self._cordsLeft,
+                       "right": self._cordsRight}
+        locates.update(locates_eng), locates.update(locates_rus)
         if locate in locates:
             func = locates[locate]
         else:
@@ -34,7 +41,7 @@ class BaseWindow(QWidget):
         return (w - s[0]) // 2, h - s[1]
 
     def _cordsTop(self, w, h, *s):
-        return (w - s[0]) // 2, 0
+        return (w - s[0]) // 2, 45
 
     def _cordsCenter(self, w, h, *s):
         return (w - s[0]) // 2, (h - s[1]) // 2
@@ -47,4 +54,3 @@ class BaseWindow(QWidget):
 
     def initButton(self):
         pass
-
