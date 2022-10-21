18f76eddb1e80bcf4eef25a41a2aacbfd2b64f56
Nybashirka <ilabon60@gmail.com>
2022-10-22 00:18:44 +0900

    Вынес Базовый класс окна в файл Windows и сделал локализацию окно через слова.

---
+++ Windows.py
@@ -0,0 +1,48 @@
+from PyQt5.QtWidgets import QWidget, QPushButton
+from win32api import GetSystemMetrics
+
+
+class BaseWindow(QWidget):
+    def __init__(self, title, **kwargs):
+        super().__init__()
+        self.initUI(title, kwargs.get("size"), kwargs.get("locate"))
+        self.initButton()
+
+    def initUI(self, title, size, locate):
+        if size is None or len(size) != 2:
+            size = [300, 300]
+        self.setGeometry(*self.locate_calc(locate, *size), *size)
+        self.setWindowTitle(title)
+
+    def locate_calc(self, locate, *size):
+        width = GetSystemMetrics(0)
+        height = GetSystemMetrics(1)
+        locates = {"в центр": self._cordsCenter,
+                   "сверху": self._cordsTop,
+                   "снизу": self._cordsBelow,
+                   "справа": self._cordsLeft,
+                   "слева": self._cordsRight}
+        if locate in locates:
+            func = locates[locate]
+        else:
+            return 0, 0
+        return func(width, height, *size)
+
+    def _cordsBelow(self, w, h, *s):
+        return (w - s[0]) // 2, h - s[1]
+
+    def _cordsTop(self, w, h, *s):
+        return (w - s[0]) // 2, 0
+
+    def _cordsCenter(self, w, h, *s):
+        return (w - s[0]) // 2, (h - s[1]) // 2
+
+    def _cordsLeft(self, w, h, *s):
+        return 0, (h - s[1]) // 2
+
+    def _cordsRight(self, w, h, *s):
+        return w - s[0], (h - s[1]) // 2
+
+    def initButton(self):
+        pass
+
--- main.py
+++ main.py
@@ -1,22 +1,6 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget, QPushButton
-from win32api import GetSystemMetrics
-
-
-class BaseWindow(QWidget):
-    def __init__(self, title, **kwargs):
-        super().__init__()
-        self.initUI(title, kwargs.get("size"))
-        self.initButton()
-
-    def initUI(self, title, size):
-        if size is None or len(size) != 4:
-            size = [300, 300, 900, 600]
-        self.setGeometry(*size)
-        self.setWindowTitle(title)
-
-    def initButton(self):
-        pass
+from PyQt5.QtWidgets import QApplication, QPushButton
+from Windows import BaseWindow
 
 
 class MainWindow(BaseWindow):
@@ -25,11 +9,8 @@ class MainWindow(BaseWindow):
 
 
 def main():
-    width = GetSystemMetrics(0) * 1.5
-    height = GetSystemMetrics(1) * 1.5
-    print(width, height)
     app = QApplication(sys.argv)
-    form = MainWindow("Входная точка", size=[(width - 300) // 2, (height - 300) // 2, 300, 300])
+    form = MainWindow("Входная точка", size=[300, 300], locate="в центр")
     form.show()
     sys.exit(app.exec())
 
